d224045e539a4532c6712e2477a99c52a2afec4b
arthur <arthur.osipenko@gmail.com>
2017-05-21 23:04:59 +0300

    add forgot email verification

--- controllers/auth/auth.router.ts
+++ controllers/auth/auth.router.ts
@@ -5,13 +5,15 @@ import { checkTokenMiddleware } from '../../middlewares/check-token.middleware';
 import { signUpHandler } from './sign-up.controller';
 import { loginHandler } from './login.controller';
 import { verifyEmailHandler } from './verify-email.controller';
+import { forgotEmailHandler } from './forgot-email-handler.controller';
 
 class AuthRouter {
     public routes = express.Router();
     private readonly configurations: IRouterConfiguration[] = [
         {type: 'post', route: '/sign-up', handler: signUpHandler},
         {type: 'post', route: '/login', handler: loginHandler},
-        {type: 'post', route: '/verify-email', middleware: [checkTokenMiddleware], handler: verifyEmailHandler}
+        {type: 'post', route: '/verify-email', middleware: [checkTokenMiddleware], handler: verifyEmailHandler},
+        {type: 'post', route: '/forgot/email', handler: forgotEmailHandler}
     ];
 
     constructor() {
---
+++ controllers/auth/forgot-email-handler.controller.ts
@@ -0,0 +1,74 @@
+import express = require('express');
+import jwt = require('jsonwebtoken');
+import Boom = require('boom');
+import Joi = require('joi');
+import { BaseController } from '../base.controller';
+import { IUserDocument, User } from '../../models/user.model';
+import { transporter } from '../../helpers/constants';
+
+class ForgotEmailHandlerController extends BaseController {
+    protected schema = Joi.object().keys({
+        email: Joi.string().email()
+    }).requiredKeys(['email']);
+
+    private user: IUserDocument;
+
+    public clearData() {
+        this.user = undefined;
+    }
+
+    public handler() {
+        this.validate();
+
+        User.findOne({email: this.req.body.email}).exec()
+            .then(this.checkUserExist.bind(this))
+            .then(this.generateToken.bind(this))
+            .then(this.saveUser.bind(this))
+            .then(this.sendForgotEmailVerification.bind(this))
+            .then(this.responseToken.bind(this))
+            .catch(this.errorHandler.bind(this));
+    }
+
+    private checkUserExist(user: IUserDocument) {
+        if (!user) {
+            throw Boom.badRequest('Email not found').output;
+        }
+        delete this.req.body.email;
+        this.user = user;
+    }
+
+    private generateToken() {
+        return this.user.createForgotPasswordToken();
+    }
+
+    private saveUser() {
+        return this.user.save();
+    }
+
+    private sendForgotEmailVerification() {
+        const mailOptions = {
+            to: this.user.email,
+            from: 'arthur.osipenko@gmail.com',
+            subject: 'Forgot password',
+            text: `Hello. This is a token for your account 
+                   ${this.user.forgotPasswordToken.value}
+                   Please go back and enter it in forgot password form.`
+        };
+        transporter.sendMail(mailOptions, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+    private responseToken() {
+        this.res.status(200).send({message: 'Email verified'});
+    }
+}
+
+export function forgotEmailHandler(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const forgotEmailHandlerController = new ForgotEmailHandlerController();
+    forgotEmailHandlerController.setHandlerParams(req, res, next);
+    forgotEmailHandlerController.clearData();
+    forgotEmailHandlerController.handler();
+}
